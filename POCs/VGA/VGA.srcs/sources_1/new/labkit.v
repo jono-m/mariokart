6e2df569c1f156815194b8caf530cad9dcbf59de
Jonathan Matthews <JMM@Jonathans-Mac-Pro.local>
2014-10-30 20:35:52 -0400

    Fixed clock issue.

--- POCs/VGA/VGA.srcs/sources_1/new/labkit.v
+++ POCs/VGA/VGA.srcs/sources_1/new/labkit.v
@@ -20,29 +20,48 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module labkit(input clk, output[3:0] vgaRed, output[3:0] vgaBlue, output[3:0] vgaGreen, output Hsync, output Vsync, output[1:0] led);
-    assign led[1] = 1;
-    assign led[0] = 0;
-    wire vga_clock;
-    clock_quarter_divider vga_clockgen(clk, vga_clock);
+module labkit(input clk, output[3:0] vgaRed, output[3:0] vgaGreen, 
+        output[3:0] vgaBlue, output Hsync, output Vsync);
+    wire clock_100mhz = clk;
+    wire clock_50mhz;
+    wire clock_25mhz;
+    clock_divider clock_divider1(clock_100_mhz, clock_50mhz);
+    clock_divider clock_divider1(clock_50_mhz, clock_25mhz);
     
-    wire [9:0] hcount;
-    wire [9:0] vcount;
-    wire hsync, vsync, at_display_area;
-    vga vga1(.vga_clock(vga_clock),.hcount(hcount),.vcount(vcount),
-          .hsync(hsync),.vsync(vsync),.at_display_area(at_display_area));
-        
-    assign vgaRed = at_display_area ? 4'b1111 : 0;
-    assign vgaGreen = at_display_area ? 4'b0000 : 0;
-    assign vgaBlue = at_display_area ? 4'b0000 : 0;
-    assign Hsync = ~hsync;
-    assign Vsync = ~vsync;
+    wire [9:0] display_x;
+    wire [8:0] display_y;
+    wire [11:0] display_rgb;
+
+    vga vga1(.vga_clock(clock_25mhz), .rgb(display_rgb), .x_coord(display_x), 
+            .y_coord(display_y), .vgaRed(vgaRed), .vgaGreen(vgaGreen), 
+            .vgaBlue(vgaBlue), .Hsync(Hsync),.Vsync(Vsync),);
+
+    assign display_rgb = 12'hF_0_0;
+
 endmodule
 
 module vga(input vga_clock,
-            output reg [9:0] hcount = 0,    // pixel number on current line
-            output reg [9:0] vcount = 0,	 // line number
-            output vsync, hsync, at_display_area);
+            input [11:0] rgb,
+            output [9:0] x_coord,
+            output [8:0] y_coord,
+            output Hsync, Vsync,
+            output [3:0] vgaRed, vgaGreen, vgaBlue);
+    reg [9:0] hcount = 0,    // pixel number on current line
+    reg [9:0] vcount = 0,     // line number
+    
+    assign Hsync = ~(hcount < 96);
+    assign Vsync = ~(vcount < 2);
+
+    wire at_display_area;
+    assign at_display_area = (hcount >= 144 && hcount < 784 && vcount >= 35 && vcount < 515);
+
+    assign vgaRed = at_display_area ? rgb[11:8] : 4'b0000;
+    assign vgaGreen = at_display_area ? rgb[7:4] : 4'b0000;
+    assign vgaBlue = at_display_area ? rgb[3:0] : 4'b0000;
+
+    assign x_coord = at_display_area ? hcount-9'd144 : 0;
+    assign y_coord = at_display_area ? (vcount[8:0])-8'd2 : 0;
+
     // Counters.
     always @(posedge vga_clock) begin
         if (hcount == 799) begin
@@ -58,19 +77,10 @@ module vga(input vga_clock,
             vcount <= vcount + 1;
         end
     end
-    
-    assign hsync = (hcount < 96);
-    assign vsync = (vcount < 2);
-    assign at_display_area = (hcount >= 144 && hcount < 784 && vcount >= 35 && vcount < 515);
 endmodule
 
-module clock_quarter_divider(input clk100_mhz, output reg clk25_mhz = 0);
-    reg [1:0] counter = 2'd0;
-    
+module clock_divider(input clk100_mhz, output reg clk25_mhz = 0);    
     always @(posedge clk100_mhz) begin
-        counter <= counter + 2'd1;
-        if (counter == 0) begin
-            clk25_mhz <= ~clk25_mhz;
-        end
+        clk25_mhz <= ~clk25_mhz;
     end
 endmodule
