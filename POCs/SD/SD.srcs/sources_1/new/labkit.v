dd283d312081eab2f3fec6dad00f9fa7879187e6
Jono Matthews <jmatth@mit.edu>
2014-11-08 14:28:58 -0500

    Added SD POC project.

---
+++ POCs/SD/SD.srcs/sources_1/new/labkit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/07/2014 02:37:18 PM
+// Design Name: 
+// Module Name: labkit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module labkit(
+
+    );
+endmodule
